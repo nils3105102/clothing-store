028b8aa5c3af9d6dbf1fb3cd12273d0da9dfd05d
Daniel <daniil.pron4enkov@gmail.com>
2020-07-02 20:16:09 +0300

    created styled components

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Homepage.scss';
+import Menu from '../../components/Menu/Menu';
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Menu />
+        </div>
+    );
+}
+
+export default Homepage;
